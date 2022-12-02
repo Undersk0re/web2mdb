5136b5b7cc9837f50a43f422b6c0ff3c07fb16b8
Undersk0re <44556359+Undersk0re@users.noreply.github.com>
2022-12-02 22:10:08 +0100

    first commit, creation

---
+++ import_one.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+from lxml import html
+from pymongo import MongoClient
+import html_to_json
+
+link= "https://www.write_where_some_webp.html" #
+
+
+# access or create new if not exists
+DBNAME="GnL"
+Collection_name="WebpSites"
+Access_string="mongodb://localhost:27017/" # standard
+myclient_conn = MongoClient(Access_string)
+html_string = requests.get(u1).text ## we can add some regex for a utf ascii filter
+d = html_to_json.convert(html_string)
+page = requests.get(link)
+mob = myclient_conn[DBNAME][Collection_name].insert_one(d)
+
+
+# print json
+for i in myclient_conn[DBNAME][Collection_name].find():
+  pprint (i)
